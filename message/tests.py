0e8c4625703d863977f3e35ce77fb73b94bbe729
Shahram Darbandi <shahramdarbandi79@gmail.com>
2024-03-02 08:37:13 +0330

    message App is Done!

---
+++ message/tests.py
@@ -0,0 +1,21 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class MessagePageTests(SimpleTestCase):
+    
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/message/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_available_by_name(self):
+        response = self.client.get(reverse('message'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse('message'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('message'))
+        self.assertContains(response, '<h1>Home</h1>')
