3217fc77f879c44e2bf31743e0c15d4c9a6582ac
Shahram Darbandi <shahramdarbandi79@gmail.com>
2024-03-02 10:16:36 +0330

    Message iS ready!

--- message/views.py
+++ message/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Message
 
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
